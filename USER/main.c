b776a21c59da72d4dc77d3d5eadc6c5315e603d3
weipeng <3214969473@qq.com>
2020-04-08 12:36:59 +0800

    CAN与USART1的通信测试通过

---
+++ USER/main.c
@@ -0,0 +1,58 @@
+#include "led.h"
+#include "delay.h"
+#include "key.h"
+#include "sys.h"
+#include "lcd.h"
+#include "usart.h"	 
+#include "can.h" 
+ 
+ 
+/************************************************
+ ALIENTEK精英STM32开发板实验25
+ CAN 实验   
+ 技术支持：www.openedv.com
+ 淘宝店铺：http://eboard.taobao.com 
+ 关注微信公众平台微信号："正点原子"，免费获取STM32资料。
+ 广州市星翼电子科技有限公司  
+ 作者：正点原子 @ALIENTEK
+************************************************/
+ 
+
+
+ int main(void)
+ {	 
+//	u16 len;
+//	u8  t=0;
+
+	delay_init();	    	 //延时函数初始化	  
+	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);//设置中断优先级分组为组2：2位抢占优先级，2位响应优先级
+	uart_init(115200);	 	//串口初始化为115200
+	LED_Init();		  		//初始化与LED连接的硬件接口
+	 	
+   
+	//CAN_Mode_Init(CAN_SJW_1tq,CAN_BS2_8tq,CAN_BS1_9tq,4,CAN_Mode_LoopBack);//CAN初始化环回模式,波特率500Kbps    
+	CAN_Mode_Init(CAN_SJW_1tq,CAN_BS2_8tq,CAN_BS1_9tq,4,CAN_Mode_Normal);//CAN普通模式初始化, 波特率500Kbps 
+	
+ 	while(1)
+	{
+
+		if(USART_RX_STA&0x8000)
+		{					   
+//			len=USART_RX_STA&0x3fff;//得到此次接收到的数据长度
+//			printf("\r\n您发送的消息为:\r\n\r\n");
+//			for(t=0;t<len;t++)
+//			{
+//				USART_SendData(USART1, USART_RX_BUF[t]);//向串口1发送数据
+//				while(USART_GetFlagStatus(USART1,USART_FLAG_TC)!=SET);//等待发送结束
+//			}
+//			printf("\r\n\r\n");//插入换行
+			UART_CAN(USART_RX_BUF);
+			USART_RX_STA=0;
+		}		
+	}
+	
+	
+}
+
+
+
